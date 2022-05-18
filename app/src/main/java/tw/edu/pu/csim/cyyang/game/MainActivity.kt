809106bd3afa614b59e645b95cd2c96a05e69602
joyyang0520 <yang10100520@gmail.com>
2022-05-18 19:49:33 +0800

    v.2

--- app/src/main/java/tw/edu/pu/csim/cyyang/game/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/cyyang/game/MainActivity.kt
@@ -1,15 +1,49 @@
 package tw.edu.pu.csim.cyyang.game
 
 import android.content.pm.ActivityInfo
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img : ImageView
+    lateinit var mysv : MySurfaceView
+    var flag:Boolean = false
+    lateinit var job : Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+        img = findViewById(R.id.img)
+        mysv = findViewById(R.id.mysv)
+
+        img.setOnClickListener({
+            if (flag){
+                flag = false
+                img.setImageResource(R.drawable.start)
+                job.cancel()
+            }
+            else{
+                flag = true
+                img.setImageResource(R.drawable.stop)
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(flag) {
+                        delay(10)
+                        var canvas: Canvas = mysv.surfaceHolder.lockCanvas()
+                        mysv.drawSomething(canvas)
+                        mysv.surfaceHolder.unlockCanvasAndPost(canvas)
+                    }
+                }
+            }
+
+        })
+
 
     }
 }
--- app/src/main/java/tw/edu/pu/csim/cyyang/game/MySurfaceView.kt
+++ app/src/main/java/tw/edu/pu/csim/cyyang/game/MySurfaceView.kt
@@ -1,6 +1,5 @@
 package tw.edu.pu.csim.cyyang.game
 
-
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
@@ -10,8 +9,10 @@ import android.view.SurfaceView
 
 class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
 SurfaceHolder.Callback{
+
     var surfaceHolder: SurfaceHolder
     var BG: Bitmap
+    var BGmove:Int = 0
 
     init {
         surfaceHolder = getHolder()
@@ -44,11 +45,24 @@ SurfaceHolder.Callback{
         var DestRect: Rect = Rect(0, 0, w, h)
         canvas.drawBitmap (BG, SrcRect, DestRect, null)
 
+        BGmove -= 5
+        var BGnew:Int = w + BGmove
+
+        if (BGnew <= 0) { //當圖片捲完時 重新開始
+            BGmove = 0
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+
+        } else {
+            DestRect = Rect(BGmove, 0, BGmove+w, h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+            DestRect = Rect(BGnew, 0, BGnew+w, h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        }
+
         val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         paint.color = Color.BLUE
         paint.textSize = 50f
         canvas.drawText("射擊遊戲(作者：楊喬茵)",50f,50f,paint)
-
 
     }
 }
